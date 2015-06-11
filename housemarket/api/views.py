143c89f3d3679435c0e1f00f78fa66d9367a22f4
Andrea Grandi <a.grandi@gmail.com>
2015-06-11 19:55:27 +0100

    More work in progress for AvgPricesView

--- housemarket/api/views.py
+++ housemarket/api/views.py
@@ -13,8 +13,14 @@ class AvgPricesView(APIView):
         date_init = datetime.strptime('2015-01-01', '%Y-%m-%d').date()
         date_stop = datetime.strptime('2015-02-28', '%Y-%m-%d').date()
 
+        # SELECT avg(price), property_type, date_of_transfer
+        #     FROM housesales_housesales
+        #     WHERE date_of_transfer >= '2015-01-01' AND date_of_transfer < '2015-02-28'
+        #     GROUP BY property_type, date_of_transfer
+        #     ORDER BY timee_of_transfer
+
         sales = HouseSales.objects.filter(
-            date_of_transfer__gte=date_init, date_of_transfer__lt=date_stop).values(
+            date_of_transfer__gte=date_init, date_of_transfer__lt=date_stop, postcode__contains='SE13').values(
             'property_type', 'date_of_transfer').order_by(
             'date_of_transfer').annotate(
             price_avg=Avg('price'))
