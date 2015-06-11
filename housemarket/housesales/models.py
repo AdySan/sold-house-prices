5c1e211d1092e40268236757c08eb53283886719
Andrea Grandi <a.grandi@gmail.com>
2015-06-11 11:47:50 +0100

    Add model for HouseSales

--- housemarket/housesales/models.py
+++ housemarket/housesales/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class HouseSales(models.Model):
+    transaction_id = models.CharField(max_length=40)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    date_of_transfer = models.DateField()
+    postcode = models.CharField(max_length=10)
+    property_type = models.CharField(max_length=1)
+    old_new = models.CharField(max_length=1)
+    duration = models.CharField(max_length=1)
+    paon = models.CharField(max_length=250)
+    saon = models.CharField(max_length=250)
+    street = models.CharField(max_length=250)
+    locality = models.CharField(max_length=250)
+    town_city = models.CharField(max_length=250)
+    district = models.CharField(max_length=250)
+    county = models.CharField(max_length=250)
+    status = models.CharField(max_length=1)
